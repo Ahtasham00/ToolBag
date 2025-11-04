c99cfefb7618d04bae63521ffbdf1663e96fe4c8
Ahtasham00 <ahtasham7776@gmail.com>
2025-11-05 01:06:19 +0500

    Added linkedin VideoDownloader

--- socailMediaVideodownloader.py
+++ socailMediaVideodownloader.py
@@ -1,18 +1,36 @@
 #isAWW
 import requests
-def tiktok_downloader(url):
-    output_file = 'temp/tiktok_video.mp4'
-    # Send a GET request to the URL
-    response = requests.get(url, stream=True)
+from bs4 import BeautifulSoup
+import json
 
-    # Check if the request was successful (status code 200)
+
+def tiktok_downloader(video_url):
+    output_file = 'temp/tiktok_video.mp4'
+    video_id = video_url.split('?')[0].split('video/')[1]
+    url=f'https://www.tikwm.com/video/media/hdplay/{video_id}.mp4'
+    response = requests.get(url, stream=True)
     if response.status_code == 200:
-        # Open a file in binary write mode to save the video
         with open(output_file, 'wb') as f:
-            # Iterate over the response content in chunks and write to the file
             for chunk in response.iter_content(chunk_size=1024):
                 f.write(chunk)
-        print("Video downloaded successfully!")
+    return output_file
+
+def likedin_video_downloader(video_url):
+    output_file = 'temp/linkedin_video.mp4'
+    content=requests.get(video_url).content
+    soup=BeautifulSoup(content,'html.parser')
+    meta_teag=soup.find_all("script")
+    if meta_teag:
+        content=meta_teag[1]
+        json_data=json.loads(content.get_text())
+        video_url=json_data.get('contentUrl')
+        response=requests.get(video_url,stream=True)
+        if response.status_code == 200:
+         with open(output_file,'wb') as f:
+             for chunk in response.iter_content(chunk_size=1024):
+                 f.write(chunk)
+         return output_file
+        else:
+            print("Video not found")   
     else:
-        print("Failed to download video. Status code:", response.status_code)
-        # Example usage
+     print("No META TAG")
